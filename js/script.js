9c20686c2e3088b00e31052b59609cfec9579d9f
Lia <aliyahrenomeron7@gmail.com>
2026-02-17 20:16:32 -0500

    added script for starting

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// event listener for space bar event
+window.addEventListener("keydown", spaceHandler);
+
+function spaceHandler(e) {
+    if (e.key === " ") {
+        document.getElementById("title").style.opacity = 0;
+        document.getElementById("start-text").style.opacity = 0;
+    }
+}
