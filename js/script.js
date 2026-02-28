bacefb0ae80d4e0634281dc9db292a44109a0eca
Lia <aliyahrenomeron7@gmail.com>
2026-02-28 11:09:30 -0500

    just testing out functionality with new page

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@ window.addEventListener("keydown", spaceHandler);
 
 function spaceHandler(e) {
     if (e.key === " ") {
-        document.getElementById("title").style.opacity = 0;
-        document.getElementById("start-text").style.opacity = 0;
+        // document.getElementById("title").style.opacity = 0;
+        // document.getElementById("start-text").style.opacity = 0;
+
+        window.location.href = "/html/second.html";
     }
 }
